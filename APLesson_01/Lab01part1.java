d4905a4d8ffa1a420fcb752403d847bc4ffd2974
vincegong67 <vincegong67@gmail.com>
2016-09-02 10:47:25 -0700

    Added files

---
+++ APLesson_01/Lab01part1.java
@@ -0,0 +1,30 @@
+public class firstProg
+{
+	public static void main(String[]arqs)
+	{
+		System.out.println("            .oo.");
+		System.out.println("          oGGGGGGo");
+		System.out.println("         GGGGGGGGGG");
+		System.out.println("  .mMMMMMMGGGGGGEEEE=");
+		System.out.println(" MMMMMMMMMMMGGEEEEEEEE");
+		System.out.println("MMMMMMMMMMMNICKEEEEEEEE");
+		System.out.println("MMMMMMMMMMMMMEEEEEEEEEE");
+		System.out.println("!MMMMMMMMMMMOOEEEEEEEE");
+		System.out.println(" MMM!MMMMMMOOOOOOE!=");
+		System.out.println("  MM!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("   MM!!!!!!!!!!!!!!!!!!!!!!!!!!'");
+		System.out.println("   !M!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("    MM!!!!!!!!!!!!!!!!!!!!!!'");
+		System.out.println("    MM!!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("    ! `!!!!!!!!!!!!!!!!!!'");
+		System.out.println("    .  !!!!!!!!!!!!!!!!!!");
+		System.out.println("       `!!!!!!!!!!!!!!'");
+		System.out.println("        !!!!!!!!!!!!!!");
+		System.out.println("        `!!!!!!!!!!'");
+		System.out.println("         !!!!!!!!!!");
+		System.out.println("         `!!!!!!'");
+		System.out.println("          !!!!!!");
+		System.out.println("          `!!'");
+		System.out.println("			!! ");
+	}
+}
---
+++ APLesson_01/firstProg.java
@@ -0,0 +1,20 @@
+public class firstProg
+{
+	public static void main(String[]arqs)
+	{
+		System.out.println("   O");
+		System.out.println("   D");
+		System.out.println("   I");
+		System.out.println("___E___");
+		System.out.println("  | |");
+		System.out.println("  | |");
+		System.out.println("  | |");
+		System.out.println("  | |");
+		System.out.println("  | |");
+		System.out.println("  | |");
+		System.out.println("  | |");
+		System.out.println("  | |");
+		System.out.println("  \\ /");
+		System.out.println("   '");
+	}
+}
