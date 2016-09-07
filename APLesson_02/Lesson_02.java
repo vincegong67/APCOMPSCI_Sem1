3f91a2472573acde85800e2b1f74297489784f95
vincegong67 <vincegong67@gmail.com>
2016-09-07 11:28:25 -0700

    Lesson 2 Finished

---
+++ APLesson_02/Lesson_02.java
@@ -0,0 +1,33 @@
+public class Lesson_02
+{
+	public static void main(String[]args)
+		{
+			System.out.println("=====> Lesson Stuff");
+			System.out.println("Exercise_01");
+			int l = 8;
+			int p = 5;
+			int recArea = l * p;
+			
+			System.out.println("The area of your rectangle is " + recArea);
+			System.out.println("");
+			System.out.println("");
+			
+			System.out.println("=====> Exercise_02");
+			int q = 12457;
+			String w = "Carmel Park Drive";
+			String e = "San Diego";
+			int r = 92130;
+			String Address= q+w+e+r;
+			System.out.println("Your Address is " + Address);
+			System.out.println("");
+			System.out.println("");
+			
+			System.out.println("=====> Exercise_03");
+			int a = 2;
+			int b = 3;
+			int c = 4;
+			int recCube= a*b+a*b+b*c+b*c+a*c+a*c;
+			System.out.println("The surface area of your RectangularCube is " + recCube);
+			
+		}
+}
---
+++ APLesson_02/Naming_Variables.java
@@ -0,0 +1,8 @@
+public class Naming_Variables
+{
+	public static void main(String[]args)
+	{
+		String name = "ProfessorHandsome";
+		System.out.println(name);
+	}
+}
