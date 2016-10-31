32889d15cd66668d9e2ba67d48da2540f572632f
vincegong67 <vincegong67@gmail.com>
2016-10-31 12:12:00 -0700

    hardasslab

---
+++ APLesson_07/Lab_07part1.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Lab_07part1
+{
+	static int number;
+	static int sum = 0;
+	
+	
+	public static void main(String[]args)
+	{	
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Please enter a number");
+		number= kb.nextInt();
+		sumDigits();
+		System.out.println("The sum of the digits in " + number + " is " + sum);
+	}
+	
+	public static void sumDigits()
+	{
+		int num = number;
+		while(num>0)
+		{
+			sum = sum + (num % 10);
+			num /= 10;
+		}
+	}
+	
+}
