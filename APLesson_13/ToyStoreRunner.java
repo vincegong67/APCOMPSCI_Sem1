8ed31e0efa3782d96ea826d729efb24952421d3d
vincegong67 <vincegong67@gmail.com>
2017-02-13 10:49:59 -0800

    fiixed

--- APLesson_13/ToyStoreRunner.java
+++ APLesson_13/ToyStoreRunner.java
@@ -3,8 +3,10 @@ public class ToyStoreRunner
 	public static void main(String[]args)
 	{
 		ToyStore list = new ToyStore("Hotwheel, Car, G.I.Joe, AF, PennyRacer, Car, Matchbox, Car, Star Wars, AF, Pullback, Car, Star Wars, AF");
+		System.out.println("List... ");
+		System.out.println("Hotwheel, Car, G.I.Joe, AF, PennyRacer, Car, Matchbox, Car, Star Wars, AF, Pullback, Car, Star Wars, AF");
 		System.out.println(list);
-		System.out.println(list.getMostFrequentToy());
-		System.out.println(list.getMostFrequentType());
+		System.out.println("Most Frequent Toy: " + list.getMostFrequentToy());
+		System.out.println("Most Frequent Type of Toy: " + list.getMostFrequentType());
 	}
 }
