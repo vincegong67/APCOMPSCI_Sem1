f43e02642a4a7286ad9cd91aa3d2455cc4487483
vincegong67 <vincegong67@gmail.com>
2017-03-29 11:44:05 -0700

    I hate the tolowercase

--- Magpie/Magpie2.java
+++ Magpie/Magpie2.java
@@ -22,6 +22,11 @@ public class Magpie2
 		{
 			System.out.println("Say something please.");
 		}
+		
+		else if (statement.trim().length() == 0)
+		{
+			response = "Say something, please.";
+		}
 		 
 		else if (findKeyword(statement, "no") >= 0)
 		{
@@ -49,13 +54,71 @@ public class Magpie2
 			response = "He sounds like a supa boosted teacher.";
 		}
 
+		// Responses which require transformations
+		else if (findKeyword(statement, "i want to", 0) >= 0)
+		{
+			response = transformIWantToStatement(statement);
+		}
+
 		else
 		{
-			response = getRandomResponse();
+			// Look for a two word (you <something> me)
+			// pattern
+			int psn = findKeyword(statement, "you", 0);
+			
+			if (psn >= 0 && findKeyword(statement, "me", psn) >= 0)
+				{
+					response = transformYouMeStatement(statement);
+				}
+			else
+				{
+					response = getRandomResponse();
+				}
 		}
 		return response;
+
 	}
 
+	
+			/**
+		* Take a statement with "I want to <something>." and transform it into
+		* "What would it mean to <something>?"
+		* @param statement the user statement, assumed to contain "I want to"
+		* @return the transformed statement
+		*/
+		private String transformIWantToStatement(String statement)
+		{
+		   statement = statement.trim();
+		   String lastChar = statement.substring(statement.length() - 1, statement.length());
+		   if(lastChar.equals("."))
+		   {
+			    statement = statement.substring(0,statement.length()-1);
+		   }
+		   int psn = findKeyword(statement, "I want to");
+		  String restOfStatement = statement.substring(psn+10, statement.length());
+		  return "What would it mean to" + restOfStatement + "?";
+		}
+
+
+		/**
+		* Take a statement with "you <something> me" and transform it into
+		* "What makes you think that I <something> you?"
+		* @param statement the user statement, assumed to contain "you" followed by "me"
+		* @return the transformed statement
+		*/
+		private String transformYouMeStatement(String statement)
+		{
+		   statement = statement.trim();
+		   String lastChar = statement.substring(statement.length() - 1, statement.length());
+		   if(lastChar.equals("."))
+		   {
+			    statement = statement.substring(0,statement.length()-1);
+		   }
+		   int psnofYou = findKeyword(statement, "you");
+		   int psnofMe = findKeyword(statement, "me", psnofYou+3);
+		   String restOfStatement = statement.substring(psnofYou+3, psnofMe-1);
+		   return "What makes you think that I " + restOfStatement + "you?";
+		}
 	/** Ex_02: The findKeyword() Method...
 	 * ========================================================= */
 	private int findKeyword(String statement, String goal, int startPos)
@@ -63,21 +126,26 @@ public class Magpie2
 		String phrase = " " + statement.toLowerCase().trim() + " ";
 		int psn = phrase.indexOf(goal,startPos);
 		
-		if(psn > 0)
+		while(psn >= 0)
 		{
-			String before =  phrase.substring(psn-1, psn);
-			String after = phrase.substring(psn + goal.length(), psn + goal.length() +1);
-			
-			if(!((before.compareTo("a") >= 0 && before.compareTo("a") <= 25) || (after.compareTo("a") >=0 && after.compareTo("a") <= 25)))
+			String before = "";
+			String after = "";
+			if (psn > 0)
+			{
+				before =  phrase.substring(psn-1, psn);
+			}
+			if(psn+goal.length() < phrase.length())
+			{
+				after = phrase.substring(psn + goal.length(), psn + goal.length() +1);
+			}
+			if (((before.compareTo("a") < 0) || (before.compareTo("z") > 0)) && ((after.compareTo("a") < 0) || (after.compareTo("z") > 0)))
 			{
 				return psn;
 			}
-			else
-			{
-				return findKeyword(phrase, goal, psn + 1);
-			}
+			
+			psn = phrase.indexOf(goal, psn + 1);
 		}
-
+ 
 		return -1;
 	}
 
