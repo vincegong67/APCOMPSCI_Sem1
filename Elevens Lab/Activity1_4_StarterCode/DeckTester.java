00f8b07880b7bcfa0952c3a5b45d6acc8518bb60
vincegong67 <vincegong67@gmail.com>
2017-05-02 10:53:05 -0700

    finished shuffler

--- Elevens Lab/Activity1_4_StarterCode/DeckTester.java
+++ Elevens Lab/Activity1_4_StarterCode/DeckTester.java
@@ -1,7 +1,8 @@
 /**
  * This is a class that tests the Deck class.
  */
-public class DeckTester {
+public class DeckTester 
+{
 
 	/**
 	 * The main method in this class checks the Deck operations for consistency.
@@ -9,17 +10,59 @@ public class DeckTester {
 	 */
 	public static void main(String[] args) 
 	{
-		String[] ranks = {"jack", "queen", "king"};
-		int[] pointValues = {11, 12, 13};
-		String[] suits = {"hearts", "diamonds"};
-		Deck d = new Deck(ranks, suits, pointValues);
-		
-		System.out.println(d);
-		d.deal();
-		System.out.println(d);
-		d.deal();
-		System.out.println(d);
-		d.deal();
-		
-	}
+		String[] r = new String[52];
+        String[] s = new String[52];
+        int[] p = new int[52];
+        for (int i = 0; i < 52; i++) 
+		{
+            String rank = "";
+            String suit = "";
+			int p = 0;
+			
+            if ((i+1) % 13 == 11) 
+			{
+                rank = "Jack";
+            }
+            else if ((i+1) % 13 == 12) 
+			{
+                rank = "Queen";
+            }
+            else if ((i+1) % 13 == 0) 
+			{
+                rank = "King";
+            }
+            else if ((i+1) % 13 == 1) 
+			{
+                rank = "Ace";
+            }
+            else 
+			{
+                rank = Integer.toString((i + 1) % 13);
+            }
+            
+            if (i < 13) 
+			{
+                suit = "Hearts";
+            }
+            else if (i < 26) 
+			{
+                suit = "Diamonds";
+            }
+            else if (i < 39) 
+			{ 
+                suit = "Clubs";
+            }
+            else 
+			{
+                suit = "Spades";
+            }
+            
+            r[i] = rank;
+            s[i] = suit;
+            p[i] = ((i+1) % 13);
+        }
+        
+        Deck d = new Deck(r, s, p);
+        System.out.println(d);
+    }
 }
