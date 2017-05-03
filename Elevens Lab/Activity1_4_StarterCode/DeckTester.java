a69e748320d40218f3ddd62f4010b7d3b9548f95
vincegong67 <vincegong67@gmail.com>
2017-05-03 10:58:15 -0700

    sd

--- Elevens Lab/Activity1_4_StarterCode/DeckTester.java
+++ Elevens Lab/Activity1_4_StarterCode/DeckTester.java
@@ -10,57 +10,9 @@ public class DeckTester
 	 */
 	public static void main(String[] args) 
 	{
-		String[] r = new String[52];
-        String[] s = new String[52];
-        int[] p = new int[52];
-        for (int i = 0; i < 52; i++) 
-		{
-            String rank = "";
-            String suit = "";
-			int p = 0;
-			
-            if ((i+1) % 13 == 11) 
-			{
-                rank = "Jack";
-            }
-            else if ((i+1) % 13 == 12) 
-			{
-                rank = "Queen";
-            }
-            else if ((i+1) % 13 == 0) 
-			{
-                rank = "King";
-            }
-            else if ((i+1) % 13 == 1) 
-			{
-                rank = "Ace";
-            }
-            else 
-			{
-                rank = Integer.toString((i + 1) % 13);
-            }
-            
-            if (i < 13) 
-			{
-                suit = "Hearts";
-            }
-            else if (i < 26) 
-			{
-                suit = "Diamonds";
-            }
-            else if (i < 39) 
-			{ 
-                suit = "Clubs";
-            }
-            else 
-			{
-                suit = "Spades";
-            }
-            
-            r[i] = rank;
-            s[i] = suit;
-            p[i] = ((i+1) % 13);
-        }
+		String[] r = new String[]{"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+        String[] s = new String[]{"Hearts", "Spades", "Diamonds", "Clubs"};
+        int[] p = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
         
         Deck d = new Deck(r, s, p);
         System.out.println(d);
